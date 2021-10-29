1c22710852642e9156bf8dbccec483d01b78e4a2
silviafado <silviafado@gmail.com>
2021-10-29 18:16:42 +0200

    Exercise2 ok

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import Panell from './components/panell/Panell';
 
-function App() {
+function App(props) {
 
-  const [budget, setBudget] = useState({ web: 0, seo: 0, ads: 0 });
+  const [budget, setBudget] = useState({ web: 0, pages: 1, lang: 1, seo: 0, ads: 0 });
   const [total, setTotal] = useState(0);
 
   const calcBudget = () => {
     let total = 0;
 
     if (budget.web) total += 500;
+    if (budget.pages > 1 || budget.lang > 1) {
+      total += budget.pages * budget.lang * 30;
+    }
     if (budget.seo) total += 300;
     if (budget.ads) total += 200;
 
     setTotal(total);
-  };
+  }
 
   useEffect(calcBudget, [budget]);
 
   return (
-    <div className="m-20">
+    <div>
       <div className="row question">¿Qué quieres hacer?</div>
       <div className="row web">
         <input name="web" type="checkbox" onChange={(event) => setBudget({ ...budget, web: event.target.checked })} /> Una página web (500€)
+        {budget.web ? <Panell onPagesChange={text => setBudget({ ...budget, pages: text })} onLangChange={text => setBudget({ ...budget, lang: text })} /> : null}
       </div>
       <div className="row seo">
         <input name="seo" type="checkbox" onChange={(event) => setBudget({ ...budget, seo: event.target.checked })} /> Una consultoria SEO (300€)
---
+++ src/components/panell/styledPanell.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StylesPanell = styled.div`
+    padding: 20px;
+    border: 3px solid black;
+    border-radius: 20px;
+    margin-top: 10px;
+    width: fit-content;
+    display: '';
+`
+
+export const StylesPages = styled.div`
+        padding-bottom: 10px;
+`
